ca044ea56f98cdb83a51bf84457823fea19ac649
Dimitri2004 <d.vanderhel@student.avans.nl>
2025-02-27 14:27:32 +0100

    hallo daar

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using schoolProject.WebApi.Models;
+using schoolProject.WebApi.Repositories;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace schoolProject.WebApi.Controllers
+{
+    [ApiController]
+    [Route("User")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserRepository _userRepository;
+        private readonly ILogger<User> _logger;
+
+        public UserController(UserRepository userRepository, ILogger<User> logger)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "Register")]
+        public async Task add(User user)
+        {
+            await _userRepository.InsertAsync(user);
+        }
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using schoolProject.WebApi.Models;
+
+namespace schoolProject.WebApi.Repositories
+{
+    public class UserRepository
+    {
+        private readonly string _sqlConnectionString;
+        public UserRepository(string sqlConnectionString)
+        {
+            _sqlConnectionString = sqlConnectionString;
+        }
+
+        public async Task<User> InsertAsync(User user)
+        {
+            using (var SqlConnection = new SqlConnection(_sqlConnectionString))
+            {
+                var environmentId = await SqlConnection.ExecuteAsync("INSERT INTO [WeatherForecast] (Id, TemperatureC, Summary) VALUES (@Id, @TemperatureC, @Summary)", user);
+                return user;
+            }
+        }
+    }
+}
