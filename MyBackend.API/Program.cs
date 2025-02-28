e7f429ee86da15ae3eead8922d856f1d898c82e0
Dimitri2004 <d.vanderhel@student.avans.nl>
2025-02-28 14:38:11 +0100

    Diagnostic homepage

--- MyBackend.API/Program.cs
+++ MyBackend.API/Program.cs
@@ -1,4 +1,4 @@
-using schoolProject.WebApi.Repositories;
+﻿using schoolProject.WebApi.Repositories;
 using Microsoft.Identity.Client;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,15 +11,24 @@ builder.Services.AddOpenApi();
 builder.Services.Configure<RouteOptions>(o => o.LowercaseUrls = true);
 
 var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+var sqlConnectionStringFound = !string.IsNullOrEmpty(sqlConnectionString);
 
-if (string.IsNullOrWhiteSpace(sqlConnectionString))
-    throw new InvalidProgramException("Configuration variable SqlConnectionString not found");
+
+//if (string.IsNullOrWhiteSpace(sqlConnectionString))
+//    throw new InvalidProgramException("Configuration variable SqlConnectionString not found");
+
+
+
+// Environment2D
+// Object2D
 
 builder.Services.AddTransient<UserRepository, UserRepository>(o => new UserRepository(sqlConnectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapOpenApi();
+
+app.MapGet("/", () => $"WebApi is up 🚀. Connectionstring found: {(sqlConnectionStringFound ? "✅" : "❌")}");
 
 app.UseHttpsRedirection();
 
